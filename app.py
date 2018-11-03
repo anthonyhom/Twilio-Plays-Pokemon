37cb506865d02b726b31375f32854b5425aef8e0
William Nicholson <williamnicholson99@gmail.com>
2018-11-03 19:08:39 -0400

    changes everything

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
-dictionary = {'up': 'w', 'down' : 's'}
+dictionary = {'up': 'w', 'down' : 's', 'left' : 'a', 'right' : 'd', 'start' : 'r', 'select' : 't', 'A' : '1', 'B' : '2', 'L' : 'q', 'R' : 'e'}
 @app.route('/sms', methods = ["GET","POST"])
 
 def sms():
@@ -13,7 +13,7 @@ def sms():
 	if body not in dictionary:
 		resp.message('\n Move/Scroll Up: W, \n Move Left: A, \n Move Right: D, \n Move/Scroll Down: S, \n Open Menu: L, \n Select: R, \n Go Back: T')
 	else:
-		player.keyPress(dictionary['message_body'])
+		player.keyPress(dictionary[body])
 	return str(resp)
 
 if __name__ == '__main__':
